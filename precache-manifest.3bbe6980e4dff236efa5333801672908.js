26aa7258adb90f5dd5de20a7592ed9fbf9a6249f
kowalskiadrian <kowalski.adrian.adam@outlook.com>
2020-10-06 14:04:25 +0200

    Updates

---
+++ precache-manifest.3bbe6980e4dff236efa5333801672908.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "13710ae6d08a38ce5ad30e5a8ece3125",
+    "url": "/leal-tattoo/index.html"
+  },
+  {
+    "revision": "c93af99b37746efaf4a3",
+    "url": "/leal-tattoo/static/css/main.e25235da.chunk.css"
+  },
+  {
+    "revision": "762d154afb96d7954792",
+    "url": "/leal-tattoo/static/js/2.f49af598.chunk.js"
+  },
+  {
+    "revision": "3377ffda74687e7745ec38a7bdbe5cdb",
+    "url": "/leal-tattoo/static/js/2.f49af598.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c93af99b37746efaf4a3",
+    "url": "/leal-tattoo/static/js/main.f54a065e.chunk.js"
+  },
+  {
+    "revision": "2b066582cd66f7ee4921",
+    "url": "/leal-tattoo/static/js/runtime-main.7cff8a93.js"
+  },
+  {
+    "revision": "ca85cd52236f4841547b6d873b819107",
+    "url": "/leal-tattoo/static/media/illustration.ca85cd52.svg"
+  },
+  {
+    "revision": "121ef34ec6cc915cf1b22d9f6c2c8324",
+    "url": "/leal-tattoo/static/media/image01.121ef34e.jpg"
+  },
+  {
+    "revision": "d63dd709504df5a1bdeb85e05419f261",
+    "url": "/leal-tattoo/static/media/image02.d63dd709.jpg"
+  },
+  {
+    "revision": "dac6ee6a953e8c796c794d58ddf653fa",
+    "url": "/leal-tattoo/static/media/image03.dac6ee6a.jpg"
+  },
+  {
+    "revision": "80dbf3a0c4b61f62a34b94610a16466b",
+    "url": "/leal-tattoo/static/media/image04.80dbf3a0.jpg"
+  },
+  {
+    "revision": "950626a0499226c325561531339edd48",
+    "url": "/leal-tattoo/static/media/image05.950626a0.jpg"
+  },
+  {
+    "revision": "a52ca08ebec055d08867299af3726f3f",
+    "url": "/leal-tattoo/static/media/image06.a52ca08e.jpg"
+  },
+  {
+    "revision": "5802c7472b0dd38b8e47798467ad0051",
+    "url": "/leal-tattoo/static/media/image07.5802c747.jpg"
+  },
+  {
+    "revision": "500a823f91e54cb60902e1ac5f0dfa69",
+    "url": "/leal-tattoo/static/media/image08.500a823f.jpg"
+  },
+  {
+    "revision": "d6b6899bb6334e6de30bf0ccffd34595",
+    "url": "/leal-tattoo/static/media/image09.d6b6899b.jpg"
+  },
+  {
+    "revision": "8d3029a713a38880bed4e2dabba6d6d8",
+    "url": "/leal-tattoo/static/media/image10.8d3029a7.jpg"
+  },
+  {
+    "revision": "76c3682525ce26bf729f12aef0b9dd80",
+    "url": "/leal-tattoo/static/media/image11.76c36825.jpg"
+  },
+  {
+    "revision": "38b1b5ec691a9c5de42761d3111e4ae3",
+    "url": "/leal-tattoo/static/media/image12.38b1b5ec.jpg"
+  },
+  {
+    "revision": "cd66ff3020dbafc684e571b565317925",
+    "url": "/leal-tattoo/static/media/image13.cd66ff30.jpg"
+  },
+  {
+    "revision": "dfd0c41855f8cfa33ed3181469078ea8",
+    "url": "/leal-tattoo/static/media/image14.dfd0c418.jpg"
+  },
+  {
+    "revision": "f1dd540ca7f94c9f0f6e2fe67e4de4b4",
+    "url": "/leal-tattoo/static/media/profile.f1dd540c.jpg"
+  }
+]);
